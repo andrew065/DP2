9b2a779e83be21d5c785fb6eb300772d8be3c794
andrewlian <andrewlian345@gmail.com>
2024-11-14 14:00:57 -0500

    minor change

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from pickle import GLOBAL
 
 from load_cell_library import Load_Cell_Sensor
 
+def read_load():
+    global sensor_val, mths_postop #updates global variables
+
+    while True:
+        sensor_val = load_sensor.get_virtual_weight(10, 1)
+        mths_postop += 1
+
+        time.sleep(0.25)
+
 #Objective 2c: Python Program
 
 #defining variables:
@@ -29,20 +38,6 @@ load_sensor = Load_Cell_Sensor()
 load_sensor.begin()
 load_sensor.zero_offset(fem_offset)
 load_sensor.set_calibration_factor(None)
-
-
-def read_load():
-    
-
-
-
-    global sensor_val, mths_postop #updates global variables
-
-    while True:
-        sensor_val = load_sensor.get_virtual_weight(10, 1)
-        mths_postop += 1
-
-        time.sleep(0.25)
 
 
 #calculating resultant tensile stress of implant stem
