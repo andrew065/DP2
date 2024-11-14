759daf7756007dba7acfefdc1010bc9566ac7ccd
miguelgheitler <miguelgheitler@gmail.com>
2024-11-14 13:20:18 -0500

    variables set up

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-hi = "Hello"
+#Objective 2c: Python Program
+
+#constant global variables
+
+#Gravitational Constant
+g = 9.81
+#assigned patient data
+age = 51
+mass = 111
+#Femoral Bone Morphology
+dia_o = 
+dia_i = 
+fem_offset = 
+#Implant Design Parameters
+dia_s = 
+E_s = 
+
+#load cell data
+sensor_val = 
+mths_postop = 
