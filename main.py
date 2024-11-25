0549e2feddf8a69ecc39cafe788d25e8eba5c4c0
andrewlian <andrewlian345@gmail.com>
2024-11-25 17:33:08 -0500

    minor changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
+from load_cell_library import Load_Cell_Sensor
 import time
 
-from load_cell_library import Load_Cell_Sensor
 
 def read_load():
     global sensor_val, mths_postop #updates global variables
@@ -11,6 +11,10 @@ def read_load():
         mths_postop += 1
 
         time.sleep(0.25)
+
+
+def femoral_stress():
+    return None
 
 #Objective 2c: Python Program
 
@@ -45,44 +49,46 @@ load_sensor.set_calibration_factor(None)
 
 def applied_load(n):
     n = sensor_val/10   #number of 10g weights
-    applied_load = mass*n*g  #force on femoral head
-    return round(applied_load,1)
+    load = mass*n*g  #force on femoral head
+    return round(load, 1)
 #calculating elastic modulus of bone
 
-def E_b(age):
-    E_b = -0.123*(age-40)+17
-    return round(E_b,1)
 
-def result_tens_stress_b(applied_load(n)):
+def em_b(age):
+    modulus_b = -0.123*(age-40)+17
+    return round(modulus_b,1)
+
+
+def result_tens_stress_b(load):
     #properties of bone
     bone_area = math.pi/4*(dia_o**2-dia_i**2)
     moment_of_inertia_bone = math.pi/64*(dia_o**4-dia_i**4)
 
     #calculating tensile stress on bone 
-    axial_stress_bone = -applied_load/bone_area
-    bending_stress_bone = (applied_load*fem_offset*dia_o/2)/moment_of_inertia_bone
+    axial_stress_bone = -load / bone_area
+    bending_stress_bone = (load * fem_offset * dia_o / 2) / moment_of_inertia_bone
 
     tensile_stress_bone = axial_stress_bone + bending_stress_bone
     
     #resultant stress of bone
-    resultant_stress_bone = tensile_stress_bone*(3*E_b(age)/(E_b(age) + E_s))**1/4
+    resultant_stress_bone = tensile_stress_bone * (3 * em_b(age) / (em_b(age) + E_s)) ** 1 / 4
 
     return round(resultant_stress_bone, 1) 
 
-    
-def result_tens_stress_s(applied_load(n)):
+
+def result_tens_stress_s(load):
     #properties of stem
     stem_area = math.pi/4*dia_s**2
     moment_of_inertia_stem = math.pi/64*dia_s**4
     
     #calculating tensile stress on stem
-    axial_stress_stem = -applied_load/stem_area
-    bending_stress_stem = (applied_load*fem_offset*(dia_s/2))/moment_of_inertia_stem #(half stem diameter is neutral axis distance)
+    axial_stress_stem = -load / stem_area
+    bending_stress_stem = (load * fem_offset * (dia_s / 2)) / moment_of_inertia_stem #(half stem diameter is neutral axis distance)
 
     tensile_stress_stem = axial_stress_stem + bending_stress_stem
     
     #resultant stress of stem
-    resultant_stress_stem = tensile_stress_stem*(1-(3*E_b/(E_b + E_s)))**1/4
+    resultant_stress_stem = tensile_stress_stem * (1 - (3 * em_b(age) / (em_b(age) + E_s))) ** 1 / 4
 
     return round(resultant_stress_stem, 1)
 
